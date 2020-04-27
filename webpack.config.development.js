2c5fe87bf70bb4c39f449f1c35cb69866017e8f0
gruf <gruf@codedrop.cc>
2020-04-27 09:28:13 +0600

    initial configs and project tree

---
+++ webpack.config.development.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: '#inline-source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'flashing.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname),
+    compress: true,
+    overlay: {
+      warnings: true,
+      errors: true
+    },
+    port: 9000
+  }
+};
